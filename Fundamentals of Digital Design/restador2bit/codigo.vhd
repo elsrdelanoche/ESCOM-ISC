b12f4cb98488f0d1885bd86d6e87ed7935d02917
Alfredo Bautista <139198090+elsrdelanoche@users.noreply.github.com>
2023-08-09 23:14:26 -0600

    Add files via upload

---
+++ Fundamentals of Digital Design/restador2bit/codigo.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity restador is
+port ( 
+	   A,B: in std_logic;
+	   R,Carry: out std_logic);
+end restador;
+
+Architecture arestador of restador is
+begin
+Carry <=((NOT A)and B);
+R<= A XOR B;
+end arestador;
