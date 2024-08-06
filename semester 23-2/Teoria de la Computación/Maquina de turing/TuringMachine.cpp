32c6880ca1e85bcceb8bd8ce29eca3e542def2aa
elsrdelanoche <alfredobr587@gmail.com>
2024-08-06 01:45:22 -0600

    Turing Machine

--- semester 23-2/Teoria de la Computación/Maquina de turing/TuringMachine.cpp
+++ semester 23-2/Teoria de la Computación/Maquina de turing/TuringMachine.cpp
@@ -4,26 +4,38 @@
 #include <tuple>
 #include <fstream>
 #include <sstream>
-#include <thread>
 #include <chrono>
+#include <thread>
 
 using namespace std;
 
+// Estructura de hash personalizada para std::pair<int, char>
+struct pair_hash {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        return hash1 ^ hash2; // Combina los hashes
+    }
+};
+
 class TuringMachine {
 public:
-    TuringMachine(vector<char> tape, int initialState, unordered_map<tuple<int, char>, tuple<int, char, int>> transitionTable)
-        : tape(tape), head(0), currentState(initialState), transitionTable(transitionTable) {}
+    TuringMachine(vector<char> tape, int initialState, unordered_map<pair<int, char>, tuple<int, char, int>, pair_hash> transitionTable)
+    : tape(tape), head(0), currentState(initialState), transitionTable(transitionTable) {}
 
     void execute() {
         cout << "Starting Turing Machine Execution..." << endl;
-        cout << "Initial Tape: ";
         printTape();
         while (true) {
-            this_thread::sleep_for(chrono::milliseconds(500)); // Pause for visualization
+            // Espera para la visualización
+            this_thread::sleep_for(chrono::milliseconds(500));
+
+            // Borra la línea de la terminal
+            clearLine();
 
             if (transitionTable.find({currentState, tape[head]}) == transitionTable.end()) {
                 cout << "\nFinal State Reached: " << currentState << endl;
-                cout << "Final Tape: ";
                 printTape();
                 return;
             }
@@ -33,9 +45,7 @@ public:
             currentState = nextState;
             head += moveDirection;
 
-            cout << "\nStep: ";
             printTape();
-            cout << "Current State: " << currentState << endl;
         }
     }
 
@@ -43,7 +53,7 @@ private:
     vector<char> tape;
     int head;
     int currentState;
-    unordered_map<tuple<int, char>, tuple<int, char, int>> transitionTable;
+    unordered_map<pair<int, char>, tuple<int, char, int>, pair_hash> transitionTable;
 
     void printTape() {
         cout << "| ";
@@ -55,11 +65,27 @@ private:
             }
         }
         cout << '|' << endl;
+
+        // Imprimir flechas de movimiento
+        cout << "  ";
+        for (int i = 0; i < tape.size(); ++i) {
+            if (i == head) {
+                cout << " ↑  ";
+            } else {
+                cout << "    ";
+            }
+        }
+        cout << endl;
+    }
+
+    void clearLine() {
+        // Mueve el cursor a la línea anterior y borra la línea
+        cout << "\033[F\033[F\033[2K";
     }
 };
 
-unordered_map<tuple<int, char>, tuple<int, char, int>> parseTransitionTable(istream& input) {
-    unordered_map<tuple<int, char>, tuple<int, char, int>> transitionTable;
+unordered_map<pair<int, char>, tuple<int, char, int>, pair_hash> parseTransitionTable(istream& input) {
+    unordered_map<pair<int, char>, tuple<int, char, int>, pair_hash> transitionTable;
     string line;
     while (getline(input, line)) {
         istringstream iss(line);
@@ -67,8 +93,6 @@ unordered_map<tuple<int, char>, tuple<int, char, int>> parseTransitionTable(istr
         char readSymbol, writeSymbol;
         if (iss >> currentState >> readSymbol >> nextState >> writeSymbol >> moveDirection) {
             transitionTable[{currentState, readSymbol}] = {nextState, writeSymbol, moveDirection};
-        } else {
-            cerr << "Error parsing line: " << line << endl;
         }
     }
     return transitionTable;
@@ -85,7 +109,7 @@ int main() {
     cin.ignore(); // Clear newline character from the input buffer
 
     vector<char> tape;
-    unordered_map<tuple<int, char>, tuple<int, char, int>> transitionTable;
+    unordered_map<pair<int, char>, tuple<int, char, int>, pair_hash> transitionTable;
     int initialState;
 
     if (choice == 1) {
@@ -101,17 +125,13 @@ int main() {
 
         cout << "Enter transition table (end with an empty line):\n";
         string line;
-        while (true) {
-            getline(cin, line);
-            if (line.empty()) break;
+        while (getline(cin, line) && !line.empty()) {
             istringstream iss(line);
             int currentState, nextState, moveDirection;
             char readSymbol, writeSymbol;
-            if (!(iss >> currentState >> readSymbol >> nextState >> writeSymbol >> moveDirection)) {
-                cerr << "Error parsing transition table." << endl;
-                continue;
+            if (iss >> currentState >> readSymbol >> nextState >> writeSymbol >> moveDirection) {
+                transitionTable[{currentState, readSymbol}] = {nextState, writeSymbol, moveDirection};
             }
-            transitionTable[{currentState, readSymbol}] = {nextState, writeSymbol, moveDirection};
         }
     } else if (choice == 2) {
         // Input from file
@@ -143,4 +163,3 @@ int main() {
 
     return 0;
 }
-
