1216398d816e598454153d642cc8fedcba45f65c
Alfredo Bautista <139198090+elsrdelanoche@users.noreply.github.com>
2024-12-11 16:18:39 -0600

    Update gamepad.ino

--- semester 25-1/05 - Arquitectura_de_Computadoras/RC-Project/gamepad.ino
+++ semester 25-1/05 - Arquitectura_de_Computadoras/RC-Project/gamepad.ino
@@ -1,3 +1,6 @@
+#include "BluetoothSerial.h"
+#include <stdlib.h> 
+
 #define FORWARD 'F'
 #define BACKWARD 'B'
 #define LEFT 'L'
@@ -9,53 +12,82 @@
 #define START 'A'
 #define PAUSE 'P'
 
+const int pinDireccion = 2;
+
+String device_name = "ESP32-BMW"; // Name for your Bluetooth device
+BluetoothSerial SerialBT;
+
 void setup() {
+  pinMode(pinDireccion, OUTPUT);
   Serial.begin(9600);  // Set the baud rate for serial communication
+  SerialBT.begin(device_name);
   // Initialize any other necessary setup code here
+  Serial.println("Bluetooth listo, conectar");
+  if (SerialBT.available()) {
+    Serial.println("Conectado, esperando...");
+  }
 }
 
 void loop() {
-  if (Serial.available()) {
-    char command = Serial.read();
+  if (SerialBT.available()) {
+    char command = SerialBT.read();
+    Serial.println(command);
     executeCommand(command);
   }
-  // Continue with other tasks in your main loop
+
 }
 
 void executeCommand(char command) {
   switch (command) {
     case FORWARD:
       // Perform action for moving forward
+      Serial.println("Adelante");
       break;
     case BACKWARD:
       // Perform action for moving backward
+      Serial.println("Atras");
       break;
     case LEFT:
       // Perform action for turning left
+      Serial.println("Izquierda");
+
       break;
+      
     case RIGHT:
       // Perform action for turning right
-      break;
+      Serial.println("Derecha");
+      while(SerialBT.read()!='0'){
+        digitalWrite(pinDireccion, HIGH);
+      }
+      digitalWrite(pinDireccion, LOW);
+    break;    
     case CIRCLE:
       // Perform action for circle
+      Serial.println("Circulo");
       break;
     case CROSS:
       // Perform action for immediate stop or crossing
+      Serial.println("Equis");
       break;
     case TRIANGLE:
       // Perform action for toggling a state (e.g., LED on/off)
+      Serial.println("Triangulo");
       break;
     case SQUARE:
       // Perform action for retrieving and sending status information
+      Serial.println("Cuadrado");
       break;
     case START:
       // Perform action for starting a process or operation
+      Serial.println("START");
       break;
     case PAUSE:
       // Perform action for pausing a process or operation
+      Serial.println("PAUSE");
       break;
     default:
       // Invalid command received
       break;
+    
   }
 }
